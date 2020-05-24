4caad91fae96503981deee4ef28c0cabb7087ca5
sabrina tsui <tsuisabrina@gmail.com>
2020-05-24 12:43:10 -0700

    add more videos and delay

--- monitor/add_videos.sh
+++ monitor/add_videos.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
+# accept user input of folder to use for monitoring
+DIR=$1
 contains_humans_dir='/home/ubuntu/videos/contains_humans_mega'
 human_less_dir='/home/ubuntu/videos/human_less_mega'
 
-simulate_dir='/home/ubuntu/cs293b-project/monitor/test_temp'
+simulate_dir='/home/ubuntu/cs293b-project/monitor/'$DIR
 
-# generate a boolean true/false
-BINARY=2
-T=1
-number=$RANDOM
-let "number %= $BINARY"
-
-if [ "$number" -eq $T ]
-#if [$RANDOM%2 -eq 0]
-then
-    video_path=$(find $contains_humans_dir -type f | shuf -n 1)
-    #echo containsHuman
-    echo $video_path
-else
-    video_path=$(find $human_less_dir -type f | shuf -n 1)
-    #echo noHuman
-    echo $video_path
-fi
-
-cp $video_path $simulate_dir
+for i in {1..10}
+   do
+       # generate a boolean true/false
+       BINARY=2
+       T=1
+       number=$RANDOM
+       let "number %= $BINARY"
+    
+       if [ "$number" -eq $T ]
+       then
+           video_path=$(find $contains_humans_dir -type f | shuf -n 1)
+           #echo containsHuman
+           echo $video_path
+       else
+           video_path=$(find $human_less_dir -type f | shuf -n 1)
+           #echo noHuman
+           echo $video_path
+       fi
+    cp $video_path $simulate_dir
+    sleep 5
+done 
 
