ea1dec5dabcb1ab2e7f1e04a6b332ab0d8670d07
Gwyneth Allwright <gwynethallwright@gmail.com>
2020-05-26 12:10:20 -0700

    Suppress tensorflow warnings and add optimization for faster classification.

--- monitor/detect_human_monitor.py
+++ monitor/detect_human_monitor.py
@@ -1,15 +1,18 @@
 # Code adapted from Tensorflow Object Detection Framework
 # https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
 # Tensorflow Object Detection Detector
+
+import logging
 import os
 import sys
 import numpy as np
-#import tensorflow as tf
 import tensorflow.compat.v1 as tf
 import cv2
 import argparse
 import smtplib
 
+logging.getLogger('tensorflow').disabled = True
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.disable_v2_behavior()
 
 def parse_command_line():
@@ -21,18 +24,14 @@ def parse_command_line():
 def alert_user(filename):
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
-
     server.ehlo()
     server.starttls()
-
     myEmail = "farmCamAlert293@gmail.com"
     myEmailPass = "richisawesome"
     destEmail = "gwynethallwright@gmail.com"
-
-    #log in to the server using user provided credentials
+    # Log in to the server using user provided credentials
     server.login(myEmail, myEmailPass)
-
-    #Send the mail
+    # Send the mail
     subject = "Human detected"
     body = "The device has detected human activity in video " + filename + "."
     message = ("From: %s\r\n" % myEmail
@@ -41,13 +40,11 @@ def alert_user(filename):
              + "\r\n"
              + body)
     server.sendmail(myEmail, destEmail, message)
-
     server.close()
 
 class DetectorAPI:
     def __init__(self, path_to_ckpt):
         self.path_to_ckpt = path_to_ckpt
-
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
@@ -55,10 +52,8 @@ class DetectorAPI:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
-
         self.default_graph = self.detection_graph.as_default()
         self.sess = tf.Session(graph=self.detection_graph)
-
         # Definite input and output Tensors for detection_graph
         self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
         # Each box represents a part of the image where a particular object was detected.
@@ -76,7 +71,6 @@ class DetectorAPI:
         (boxes, scores, classes, num) = self.sess.run(
             [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
             feed_dict={self.image_tensor: image_np_expanded})
-
         im_height, im_width,_ = image.shape
         boxes_list = [None for i in range(boxes.shape[1])]
         for i in range(boxes.shape[1]):
@@ -84,7 +78,6 @@ class DetectorAPI:
                         int(boxes[0,i,1]*im_width),
                         int(boxes[0,i,2] * im_height),
                         int(boxes[0,i,3]*im_width))
-
         return boxes_list, scores[0].tolist(), [int(x) for x in classes[0].tolist()], int(num[0])
 
     def close(self):
@@ -104,34 +97,28 @@ if __name__ == "__main__":
     print("Started processing for " + filename)
     cap = cv2.VideoCapture(videos_path + filename)
     num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    #fps = cap.get(cv2.CAP_PROP_FPS)    
-    #duration = num_frames/fps
     if num_frames <= 0 :
         print("Video could not be read.")
         sys.exit()
     humanCount = 0
     frame_num = 0
     while True:
+
         r, img = cap.read()
 
         if r==False:
             print("Human not found in "+filename)
             break
 
-        if True: 
-        #(frame_num % skip_frames) == 0:
+        img = cv2.resize(img, (x_dim, y_dim))
+        boxes, scores, classes, num = odapi.processFrame(img)
 
-            img = cv2.resize(img, (x_dim, y_dim))
-            boxes, scores, classes, num = odapi.processFrame(img)
-
-            for i in range(len(boxes)):
-                # Class 1 represents human
-                if classes[i] == 1 and scores[i] > threshold:
-                    humanCount = humanCount+1
+        for i in range(len(boxes)):
+            # Class 1 represents human
+            if classes[i] == 1 and scores[i] > threshold:
+                humanCount = humanCount+1
 
             if humanCount > humanCountThreshold:
                 print("Human found in "+filename)
                 alert_user(filename)
-                break
-
-        frame_num = frame_num + 1
+                sys.exit()
